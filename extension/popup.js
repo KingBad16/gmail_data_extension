6b1f63e54ba532651bfa206a988924b36fbfe518
KingBad16 <sambad.majumder1610@gmail.com>
2026-05-19 10:21:50 +0530

    upated popup

--- extension/popup.js
+++ extension/popup.js
@@ -1,1 +1,12 @@
 console.log("This is the popup script for the extension.");
+
+// Add click event to the login button
+document.addEventListener('DOMContentLoaded', function() {
+	const loginBtn = document.getElementById('loginBtn');
+	if (loginBtn) {
+		loginBtn.addEventListener('click', function() {
+			// This demonstrates popup.js is running on click
+			alert('popup.js is running!');
+		});
+	}
+});
